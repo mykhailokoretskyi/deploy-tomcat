259f2ab14e2d10cf09d9841a4413d5af08d453d2
Mykhailo Koretskyi <radio.mkor@gmail.com>
2017-03-03 22:23:23 -0300

    Spring app

--- app/src/main/java/com/radiomkor/controller/HelloWorld.java
+++ app/src/main/java/com/radiomkor/controller/HelloWorld.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HelloWorld {
+    @RequestMapping("/")
     public String hello(){
         return "Hello World app";
     }
